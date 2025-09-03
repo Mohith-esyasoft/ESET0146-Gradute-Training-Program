502df0013624f9638eae05a02f2f15c22f3a43e2
Mohith-Esyasoft <Vatti.Mohith@esyasoft.com>
2025-09-03 14:11:02 +0530

    Initial commit of local files

---
+++ Basics of Python/test2/area_calcultor.py
@@ -0,0 +1,7 @@
+import math
+area = input ("enter the area: ")
+side = area ** .05
+if side == int(side):
+print("it is a prefect square ")
+  
+
---
+++ Basics of Python/test2/main.py
@@ -0,0 +1,7 @@
+help = "sybau"
+
+result = print(help)
+
+mogh = "foul tarnished"
+print(len(mogh)) 
+
