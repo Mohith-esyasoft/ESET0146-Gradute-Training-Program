b5569d58c264b31c331ec0d7c2a55c1cefbcffba
Mohith-Esyasoft <Vatti.Mohith@esyasoft.com>
2025-09-03 14:12:49 +0530

    Commit changes in the my-app submodule

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome tarnished
         </a>
       </header>
     </div>
