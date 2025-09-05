5b0ab26435760ae1112945023fa12e8610eb2748
Mohith-Esyasoft <Vatti.Mohith@esyasoft.com>
2025-09-05 09:49:55 +0530

    assignments

---
+++ Basics of Python/Day2 Assignment/main.py
@@ -0,0 +1,6 @@
+points = [(1, 2), (5, 8), (3, 4)]
+for x, y in points:
+    if x > y:
+        print(f"The X coordinate is greater than the Y coordinate for point ({x}, {y}).")
+    else:
+        print(f"The Y coordinate is greater than or equal to the X coordinate for point ({x}, {y}).")
